3a724a67c496d3df7c02b6d4da4d59f3caba4d0a
A-UYANAEV <A-UYANAEV@DESKTOP-29CFBJD>
2021-09-28 20:40:42 +0300

    Add project files.

---
+++ ChallangeManager/Controllers/ChallangeController.cs
@@ -0,0 +1,22 @@
+﻿using ChallangeManager.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallangeManager.Controllers
+{
+    [Route("challange")]
+    [ApiController]
+    public class ChallangeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getAll")]
+        public IEnumerable<Challange> GetChallanges()
+        {
+            return Enumerable.Empty<Challange>();
+        }
+    }
+}
---
+++ ChallangeManager/Model/Challange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallangeManager.Model
+{
+    public class Challange
+    {
+        public string Name { get; set; }
+
+        public DateTime DeadLine { get; set; }
+    }
+}
