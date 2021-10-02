c547d53b108d9ca1d127596fcee9ded0dd130090
A-UYANAEV <A-UYANAEV@DESKTOP-29CFBJD>
2021-10-02 17:01:18 +0300

    add test notificationService

---
+++ NotificationService/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace NotificationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpClient Smtp = new SmtpClient("smtp.gmail.com", 587);
+            Smtp.Credentials = new NetworkCredential("uyanaev1911@gmail.com", "xxxxxxxxx.");
+            MailMessage Message = new MailMessage();
+            Message.From = new MailAddress("uyanaev1911@gmail.com");
+            Message.To.Add(new MailAddress("uyanaev00@gmail.com"));
+            Message.Subject = "тема";
+            Message.Body = "сообщение";
+
+            try
+            {
+                Smtp.Send(Message);
+            }
+            catch (SmtpException e)
+            {
+
+            }
+        }
+    }
+}
